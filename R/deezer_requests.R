33c41c3dd83212d80e5d8d6b0d3fe3da0a02664c
Molitor Corentin <corentin.molitor@gmail.com>
2024-12-23 22:53:06 +0100

    Added Bubble plot + Correcte bug genres
     - Genres were duplicated when creating a dataframe to plot the timeline, now the request_albums function will have a list of genres
    
     - Created a bubble chart to plot the popular albums per genre per year!

--- R/deezer_requests.R
+++ R/deezer_requests.R
@@ -12,11 +12,12 @@ request_playlist_info <- function(playlist_id = "13323984883") {
   
   # If length 1 it means that playlist_info only contains "$error"
   if(length(playlist_info) == 1) {
-    stop(paste0("Error while retrieving the playlist, error code: ", playlist_info$error$code))
+    stop(paste0("Error while retrieving the playlist: ", playlist_info$error$code))
   } else {
     return(playlist_info)
   }
 }
+
 
 #' Get playlist tracks:
 #' Makes a request to Deezer's API to get the tracks as a parsed R object.
@@ -26,12 +27,13 @@ request_playlist_info <- function(playlist_id = "13323984883") {
 #' @export
 #' @examples request_tracks("13323984883")
 request_tracks <- function(playlist_id = "13323984883") {
-  tracks_request <- httr::GET(paste0("https://api.deezer.com/playlist/", playlist_id, "/tracks/"))
+  tracks_request <- httr::GET(paste0("https://api.deezer.com/playlist/", 
+                                     playlist_id, "/tracks/"))
   tracks_content <- httr::content(x = tracks_request, as = "parsed")
   
   # If length 1 it means that playlist_info only contains "$error"
   if(length(tracks_content) == 1) {
-    stop(paste0("Error while retrieving the tracks, error code: ", tracks_content$error$code))
+    stop(paste0("Error while retrieving the tracks: ", tracks_content$error$code))
   } else {
     tracks_list <- tracks_content$data
     
@@ -49,44 +51,63 @@ request_tracks <- function(playlist_id = "13323984883") {
   }
 }
 
+
 #' Get the albums information:
 #' Makes requests to Deezer's API to get the albums as a data.frame
 #' Needed to get the genres, year of release and explicit lyrics information.
 #'
-#' @param tracks a dataframe with an "artist" column, can be obtained with [DeeR::request_tracks]
+#' @param tracks a dataframe with an "artist" column, can be obtained with 
+#'               [DeeR::request_tracks]
 #' @return A data.frame with the albums information
 #' @export
 request_albums <- function(tracks) {
+  
   album_lists <- list()
+  print("Requesting Albums info, this can take a while...")
   
-  cat("Requesting Albums info, this can take a while...")
+  unique_albums <- unique(tracks[tracks$readable == TRUE, "album"])
   
-  for(album in unique(tracks$album)){
+  # Setting up a progress bar:
+  pb <- txtProgressBar(max = length(unique_albums), style = 3)
+  
+  for(album in unique_albums){
     album_request <- httr::GET(paste0("https://api.deezer.com/album/", album$id))
     album_content <- httr::content(album_request)
     
     # Extract the list of genres from the album:
-    genres <- c()
-    
+    genres <- list()
     if(length(album_content$genres$data) > 0){
       for(i in c(1:length(album_content$genres$data))){
-        genres <- c(genres, album_content$genres$data[[i]]$name)
+        genres <- append(genres, album_content$genres$data[[i]]$name)
       }
     } else { genres <- "no genre" }
     
-    if(length(album_content$release_date) == 0) { album_content$release_date <- "0000-00-00" }
-    if(length(album_content$explicit_lyrics) == 0) { album_content$explicit_lyrics <- "Unknown" }
+    if(length(album_content$title) == 0)           album_content$title <- "no title"
+    if(length(album_content$release_date) == 0)    album_content$release_date <- "0000-00-00"
+    if(length(album_content$explicit_lyrics) == 0) album_content$explicit_lyrics <- "Unknown"
+    if(length(album_content$label) == 0)           album_content$label <- "Unknown"
+    if(length(album_content$nb_tracks) == 0)       album_content$nb_tracks <- "Unknown"
+    if(length(album_content$fans) == 0)            album_content$fans <- "Unknown"
     
-    album_df <- data.frame(genre = genres,
+    album_df <- data.frame(title        = album_content$title,
+                           genre        = I(list(genres)),
                            release_date = album_content$release_date,
-                           explicit = album_content$explicit_lyrics)
+                           explicit     = album_content$explicit_lyrics,
+                           label        = album_content$label,
+                           nb_tracks    = album_content$nb_tracks,
+                           duration     = album_content$duration,
+                           fans         = album_content$fans)
     
     album_lists <- append(album_lists, list(album_df))
+    
+    setTxtProgressBar(pb, pb$getVal()+1)
     
     # Test with sleep to respect the API quotas (50 requests / second)
     # Todo: find a better way to implement this:
     Sys.sleep(0.021)
   }
+  
+  close(pb)
   
   return(data.frame(do.call("rbind", album_lists)))
 }
--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,7 +1,7 @@
-#' Will plot the top n artists (in number of songs in the playlist).
+#' Plot the top n artists (in number of songs in the playlist).
 #'
-#' @param tracks a dataframe with an "artist" column, can be obtained with [DeeR::request_tracks]
-#' @param n the number of artists to plot, default: 10
+#' @param tracks A dataframe with an "artist" column, can be obtained with [DeeR::request_tracks]
+#' @param n The number of artists to plot, default: 10
 #' @return A ggplot barplot with the top artists by number of songs
 #' @export
 plot_top_artists <- function(tracks, n = 10) {
@@ -13,22 +13,27 @@ plot_top_artists <- function(tracks, n = 10) {
   
   top_artists <- data.frame(table(artists$name)[order(table(artists$name), decreasing = T)[1:n]])
   
-  gg_top_artists <- ggplot2::ggplot(data = top_artists, ggplot2::aes_string(x = "Var1", y = "Freq")) +
-                      ggplot2::geom_bar(col = "black", fill = "deepskyblue4", alpha = 0.7, stat = "identity") + 
-                      ggplot2::theme_bw() + ggplot2::xlab("") + ggplot2::ylab("Number of songs") +
-                      ggplot2::ggtitle(paste0("Top artists (", length(unique(artists$name)), " artists)")) +
-                      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5))
+  gg_top_artists <- ggplot2::ggplot(data = top_artists, 
+                                    ggplot2::aes_string(x = "Var1", y = "Freq")) +
+                    ggplot2::geom_bar(col = "black", 
+                                      fill = "deepskyblue4", 
+                                      alpha = 0.7,
+                                      stat = "identity") + 
+                    ggplot2::theme_bw() + ggplot2::xlab("") + 
+                    ggplot2::ylab("Number of songs") +
+                    ggplot2::ggtitle(paste0("Top artists (", length(unique(artists$name)), " artists)")) +
+                    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5))
   return(gg_top_artists)
 }
 
 
-#' Will plot the genres present in the playlist as a treemap.
+#' Plot the genres present in the playlist as a treemap.
 #'
-#' @param albums a data.frame obtained with [DeeR::request_albums]
+#' @param albums A data.frame obtained with [DeeR::request_albums]
 #' @return A treemap of the genres present in the playlist
 #' @export
 plot_genres_treemap <- function(albums) {
-  treemap_data <- data.frame(table(albums$genre))
+  treemap_data <- data.frame(table(unlist(albums$genre)))
   
   t <- treemap::treemap(dtf = treemap_data, 
                         index = c("Var1"),
@@ -38,37 +43,47 @@ plot_genres_treemap <- function(albums) {
                         fontface.labels = c(2, 1), # 2 = bold, 1 = normal
                         bg.labels = c("transparent"),
                         align.labels = list(c("center", "center"),
-                                           c("center", "center")),
+                                            c("center", "center")),
                         inflate.labels = FALSE,
                         title = "Genres - Treemap")
   return(t)
 }
 
 
-#' Will plot the release dates of the albums present in the playlist.
+#' Plot the release dates of the albums present in the playlist.
 #'
-#' @param albums a data.frame obtained with [DeeR::request_albums]
+#' @param albums A data.frame obtained with [DeeR::request_albums]
 #' @return A plot of the release dates
 #' @export
 plot_timeline <- function(albums) {
-  release_years <- substr(x = albums$release_date, start = 1, stop = 4)
+  
+  release_years <- as.numeric(substr(x = albums$release_date, start = 1, stop = 4))
   timespan <- seq(min(release_years[release_years != "0000"]), max(release_years), by = 1)
   
-  time_df <- as.data.frame(table(release_years))
+  # Keeping only the first "genre" of every album: possible source of bias:
+  time_df <- data.frame(year = release_years,
+                        genre = unlist(lapply(albums$genre, '[[', 1)))
   
-  gg_time <- ggplot2::ggplot(data = time_df, ggplot2::aes_string(x = "release_years", y = "Freq")) +
-    ggplot2::geom_bar(col = "black", fill = "deepskyblue4", alpha = 0.7, stat = "identity") + 
-    ggplot2::theme_bw() + ggplot2::ylab("Number of albums") +
-    ggplot2::ggtitle(paste0("Release Year (", nrow(time_df), " incredible albums)")) + 
-    ggplot2::scale_x_discrete(name = "Release year", breaks = timespan, drop = FALSE)
+  gg_time <- ggplot2::ggplot(time_df, ggplot2::aes_string(x = "year", fill = "genre")) +
+               ggplot2::geom_dotplot(col = "black",
+                                     alpha = 0.7,
+                                     dotsize = 0.7,
+                                     binwidth = 1,
+                                     drop = FALSE,
+                                     stackgroups = TRUE) +
+              ggplot2::theme_bw() + 
+              ggplot2::ggtitle(paste0("Release Year (", nrow(time_df), " incredible albums)")) + 
+              ggplot2::scale_x_continuous(name = "Release year", breaks = time_df$year) +
+              ggplot2::scale_fill_discrete(breaks = names(head(sort(table(time_df$genre), decreasing = T), 5))) +
+              ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5))
   
   return(gg_time)
 }
 
-#' Will plot a wordcloud of the words present in the tracks title
+#' Plot a wordcloud of the words present in the tracks title
 #'
-#' @param tracks a data.frame obtained with [DeeR::request_tracks]
-#' @param n the maximum number of words to plot
+#' @param tracks A data.frame obtained with [DeeR::request_tracks]
+#' @param n The maximum number of words to plot
 #' @return A wordcloud of top n most frequent words
 #' @export
 plot_titles_wordcloud <- function(tracks, n = 50) {
@@ -87,7 +102,6 @@ plot_titles_wordcloud <- function(tracks, n = 50) {
   title_words <- gsub(pattern = "^don$|^the$|^of$|^on$|^in$|^it$|^if$|^to$|^so$|^for$|^les$|^des$|^le$|^sur$|^dans$", 
                       replacement = "", x = title_words, ignore.case = TRUE)
 
-  
   wc <- wordcloud::wordcloud(words = title_words, 
                              max.words = n, 
                              min.freq = 1,  
@@ -98,8 +112,53 @@ plot_titles_wordcloud <- function(tracks, n = 50) {
   return(wc)
 }
 
+#' #' Will plot the distribution of the BPMs in a playlist as a beeswarm !
+#' #'
+#' #' @param tracks A data.frame obtained with [DeeR::request_tracks]
+#' #' @return a beeswarm of the BPMs
+#' #' @export
+#' plot_BPMs <- function(tracks) {
+#'   beeswarm(x = iris$Sepal.Length,
+#'     pch = 16,
+#'     col = "blue")
+#' }
+
 # fans
+# The rank is a global indicator of a song's popularity on DEezer, who goes from 0 to 1M. 
 
-# bpm
+# Albums mosaic ?
 
-# Albums mosaic
+#' Plot a bubble chart of the number of fans per album 
+#'
+#' @param albums A data.frame obtained with [DeeR::request_albums]
+#' @return A bubble plot of the albums per genre with size corresponding to the number of fans
+#' @export
+plot_bubble_fans <- function(albums) {
+  
+  release_years <- as.numeric(substr(x = albums$release_date, start = 1, stop = 4))
+  
+  bubble_df <- data.frame(year = release_years,
+                          genre = unlist(lapply(albums$genre, '[[', 1)),
+                          fans = albums$fans / 1000)
+  gg_bubble <- 
+  ggplot2::ggplot(bubble_df, aes_string(x = "year", y = "genre", col = "genre", size = "fans")) + 
+    ggplot2::geom_point(alpha = 0.5) +
+    ggplot2::theme_bw() +
+    ggplot2::ggtitle(paste0("Release Year (", nrow(bubble_df), " incredible albums) - Number in 1000s of fans")) + 
+    ggplot2::scale_x_continuous(name = "Release year", breaks = bubble_df$year) +
+    ggplot2::scale_color_discrete(breaks = "") +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5)) +
+    ggplot2::scale_size(range = c(1, 15), name = "Fans")
+  
+  return(gg_bubble)
+}
+
+# # Duration + number of tracks heatmap
+# 
+# # Waffle chart of labels ?
+# plot_top_labels <- function(albums, n = 8) {
+#   
+#   top_labels <- as.data.frame(head(sort(x = table(albums$label), decreasing = T), n))
+#   
+#   waffle::waffle(top_labels)
+# }
